51d153999f78e622f23cee34e1b6480344ae163d
李业 <liye@xiangshangban.com>
2018-02-08 11:56:26 +0800

    liye

--- src/main/java/com/xiangshangban/transit_service/service/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.xiangshangban.transit_service.service;
-
-import com.xiangshangban.transit_service.bean.Department;
-
-public interface DepartmentService {
-   
-    int insertDepartment(Department department);
-  
-}
