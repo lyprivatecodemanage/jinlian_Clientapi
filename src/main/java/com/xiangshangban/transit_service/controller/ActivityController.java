923155dd3e69bf6defafc44a86b14b20caa7d305
李业 <liye@xiangshangban.com>
2018-02-09 11:37:53 +0800

    liye

--- src/main/java/com/xiangshangban/transit_service/controller/ActivityController.java
+++ src/main/java/com/xiangshangban/transit_service/controller/ActivityController.java
@@ -1,5 +1,187 @@
 package com.xiangshangban.transit_service.controller;
 
-public class ActivityController {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.xiangshangban.transit_service.util.EmptyUtil;
+
+@RestController
+@RequestMapping("ActivityController")
+public class ActivityController {
+	private SimpleDateFormat dateSdf = new SimpleDateFormat("yyyy-MM-dd");
+	private SimpleDateFormat hourSdf = new SimpleDateFormat("mm:ss");
+	
+	/**
+	 * 4.1新增主题
+	 * @param jsonString
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/addTask",produces="application/json;charset=utf-8",method=RequestMethod.POST)
+	public Map<String,Object> addTask(@RequestBody String jsonString,HttpServletRequest request){
+		Map<String,Object> result = new HashMap<String,Object>();
+		List<String> params = new ArrayList<String>();
+		if(StringUtils.isEmpty(jsonString)){
+			result.put("returnCode", "3006");
+			result.put("message", "必传参数为空");
+			return result;
+		}
+		JSONObject jobj = JSON.parseObject(jsonString);
+		String token = request.getHeader("token"); 		
+		String templateLevel = jobj.getString("templateLevel");//优先级  0：低， 1：高
+		String imageKey = jobj.getString("imageKey");//图片唯一名称，通过图片上传接口获得;可传多个图片key,以逗号分隔
+		String broadStartDate = jobj.getString("broadStartDate");//开始日期
+		String broadEndDate = jobj.getString("broadEndDate");//结束日期
+		String broadStartTime = jobj.getString("broadStartTime");//开始时间
+		String broadEndTime = jobj.getString("broadEndTime");//结束时间
+		String roastingTime = jobj.getString("roastingTime");//轮播时间间隔 ，单位为秒
+		String doorId = jobj.getString("roastingTime");//门ID
+		params.add(token);
+		params.add(templateLevel);
+		params.add(imageKey);
+		params.add(broadStartDate);
+		params.add(broadEndDate);
+		params.add(broadStartTime);
+		params.add(broadEndTime);
+		params.add(roastingTime);
+		params.add(doorId);
+		boolean isEmpty = EmptyUtil.isEmpty(params);
+		if(!isEmpty){
+			result.put("message", "必传参数为空");
+			result.put("returnCode", "3006");
+			return result;
+		}
+		try {
+			broadStartDate = dateSdf.format(dateSdf.parse(broadStartDate));
+			broadEndDate = dateSdf.format(dateSdf.parse(broadEndDate));
+			broadStartTime = hourSdf.format(hourSdf.parse(broadStartTime));
+			broadEndTime = hourSdf.format(hourSdf.parse(broadEndTime));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result.put("message", "日期时间格式错误");
+			result.put("returnCode", "9999");
+			return result;
+		}
+		if(RegisterController.Token.equals(token)){
+			result.put("message", "操作成功");
+			result.put("returnCode", "3000");
+			result.put("templateId", "12358555");//添加成功时返回主题ID
+			return result;
+		}else{
+			result.put("message", "token验证失败");
+			result.put("returnCode", "9999");
+			return result;
+		}
+	}
+	/**
+	 * 4.2修改主题
+	 * @param jsonString
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/updateTask",produces="application/json;charset=utf-8",method=RequestMethod.POST)
+	public Map<String,Object> updateTask(@RequestBody String jsonString,HttpServletRequest request){
+		Map<String,Object> result = new HashMap<String,Object>();
+		List<String> params = new ArrayList<String>();
+		if(StringUtils.isEmpty(jsonString)){
+			result.put("returnCode", "3006");
+			result.put("message", "必传参数为空");
+			return result;
+		}
+		JSONObject jobj = JSON.parseObject(jsonString);
+		String token = request.getHeader("token"); 		
+		String templateId = jobj.getString("templateId");//主题ID
+		String templateLevel = jobj.getString("templateLevel");//优先级  0：低， 1：高
+		String imageKey = jobj.getString("imageKey");//图片唯一名称，通过图片上传接口获得;可传多个图片key,以逗号分隔
+		String broadStartDate = jobj.getString("broadStartDate");//开始日期
+		String broadEndDate = jobj.getString("broadEndDate");//结束日期
+		String broadStartTime = jobj.getString("broadStartTime");//开始时间
+		String broadEndTime = jobj.getString("broadEndTime");//结束时间
+		String roastingTime = jobj.getString("roastingTime");//轮播时间间隔 ，单位为秒
+		params.add(token);
+		params.add(templateId);
+		params.add(templateLevel);
+		params.add(imageKey);
+		params.add(broadStartDate);
+		params.add(broadEndDate);
+		params.add(broadStartTime);
+		params.add(broadEndTime);
+		params.add(roastingTime);
+		boolean isEmpty = EmptyUtil.isEmpty(params);
+		if(!isEmpty){
+			result.put("message", "必传参数为空");
+			result.put("returnCode", "3006");
+			return result;
+		}
+		try {
+			broadStartDate = dateSdf.format(dateSdf.parse(broadStartDate));
+			broadEndDate = dateSdf.format(dateSdf.parse(broadEndDate));
+			broadStartTime = hourSdf.format(hourSdf.parse(broadStartTime));
+			broadEndTime = hourSdf.format(hourSdf.parse(broadEndTime));
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result.put("message", "日期时间格式错误");
+			result.put("returnCode", "9999");
+			return result;
+		}
+		if(RegisterController.Token.equals(token)){
+			result.put("message", "操作成功");
+			result.put("returnCode", "3000");
+			return result;
+		}else{
+			result.put("message", "token验证失败");
+			result.put("returnCode", "9999");
+			return result;
+		}
+	}
+	/**
+	 * 4.3 删除主题
+	 * @param jsonString
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/deleteTask",produces="application/json;charset=utf-8",method=RequestMethod.POST)
+	public Map<String,Object> deleteTask(@RequestBody String jsonString,HttpServletRequest request){
+		Map<String,Object> result = new HashMap<String,Object>();
+		List<String> params = new ArrayList<String>();
+		if(StringUtils.isEmpty(jsonString)){
+			result.put("returnCode", "3006");
+			result.put("message", "必传参数为空");
+			return result;
+		}
+		JSONObject jobj = JSON.parseObject(jsonString);
+		String token = request.getHeader("token"); 		
+		String templateId = jobj.getString("templateId");//主题ID
+		params.add(token);
+		params.add(templateId);
+		boolean isEmpty = EmptyUtil.isEmpty(params);
+		if(!isEmpty){
+			result.put("message", "必传参数为空");
+			result.put("returnCode", "3006");
+			return result;
+		}
+		if(RegisterController.Token.equals(token)){
+			result.put("message", "操作成功");
+			result.put("returnCode", "3000");
+			return result;
+		}else{
+			result.put("message", "token验证失败");
+			result.put("returnCode", "9999");
+			return result;
+		}
+	}
 }
